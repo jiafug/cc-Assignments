0e94acbc41ad74ba109690269f376cbed3b07a32
Moritz Reich <moritz.reich@minest.de>
2021-11-04 11:13:35 +0100

    added ssh keys upload

--- assignments/01/create-gcp.sh
+++ assignments/01/create-gcp.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+# TODO: Change to a wildcard approach for the public key; currently it is using the first command argument
+# Upload public key to gcloud
+gcloud compute os-login ssh-keys add --key-file=$1 --ttl=120d
+
 # Create a gcloud compute instance with the following settings:
 # - name: test-instance
 # - image: Ubuntu 18.04
