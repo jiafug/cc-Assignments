87c0b660f88fe5e975e71b62363bb648db6d9c51
jiafug <comet.liu@protonmail.com>
2022-02-09 14:09:04 +0100

    task 1

---
+++ assignments/05/WordCount.java
@@ -0,0 +1,161 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.tuberlin;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.MultipleParameterTool;
+import org.apache.flink.core.fs.FileSystem;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.ProcessAllWindowFunction;
+import org.apache.flink.streaming.api.windowing.assigners.EventTimeSessionWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
+import org.apache.flink.util.Preconditions;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Implements the "WordCount" program that computes a simple word occurrence histogram over text
+ * files in a streaming fashion.
+ *
+ * <p>The input is a plain text file with lines separated by newline characters.
+ *
+ * <p>Usage: <code>WordCount --input &lt;path&gt; --output &lt;path&gt;</code><br>
+ * If no parameters are provided, the program is run with default data from {@link}.
+ *
+ * <p>This example shows how to:
+ *
+ * <ul>
+ *   <li>write a simple Flink Streaming program,
+ *   <li>use tuple data types,
+ *   <li>write and use user-defined functions.
+ * </ul>
+ */
+public class WordCount {
+
+    // *************************************************************************
+    // PROGRAM
+    // *************************************************************************
+
+    public static void main(String[] args) throws Exception {
+
+        // Checking input parameters
+        final MultipleParameterTool params = MultipleParameterTool.fromArgs(args);
+
+        // set up the execution environment
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        // set the streaming mode to Batch to signalize the finite input
+
+        // make parameters available in the web interface
+        env.getConfig().setGlobalJobParameters(params);
+
+        // get input data
+        DataStream<String> text = null;
+        if (params.has("input")) {
+            // union all the inputs from text files
+            for (String input : params.getMultiParameterRequired("input")) {
+                if (text == null) {
+                    text = env.readTextFile(input);
+                } else {
+                    text = text.union(env.readTextFile(input));
+                }
+            }
+            Preconditions.checkNotNull(text, "Input DataStream should not be null.");
+        } else {
+            System.out.println("No input data set provided.");
+            System.out.println("Use --input to specify file input.");
+            // get default test text data
+        }
+
+        DataStream<Tuple2<String, Integer>> counts =
+                // split up the lines in pairs (2-tuples) containing: (word,1)
+                text.flatMap(new Tokenizer())
+                        // group by the tuple field "0" and sum up tuple field "1"
+                        .keyBy(value -> value.f0).sum(1)
+                        // filter words out that contain non-alphabetical characters
+                        .filter(value -> value.f0.chars().allMatch(Character::isLetter))
+                        // create a window over all items
+                        .windowAll(EventTimeSessionWindows.withGap(Time.seconds(10)))
+                        // process all items in the window / sort them by value
+                        .process(new ProcessAllWindowFunctionWithSort());
+
+        // emit result
+        if (params.has("output")) {
+            counts.writeAsCsv(params.get("output"), FileSystem.WriteMode.OVERWRITE, "\n", ",");
+        } else {
+            System.out.println("Printing result to stdout. Use --output to specify output path.");
+            counts.print();
+        }
+        // execute program
+        env.execute("Streaming WordCount");
+    }
+
+    // *************************************************************************
+    // USER FUNCTIONS
+    // *************************************************************************
+
+
+    private static class ProcessAllWindowFunctionWithSort extends ProcessAllWindowFunction<Tuple2<String, Integer>, Tuple2<String, Integer>, TimeWindow> {
+        private final Map<String, Integer> map;
+
+        private ProcessAllWindowFunctionWithSort() {
+            this.map = new HashMap<String, Integer>();
+        }
+
+        @Override
+        public void process(ProcessAllWindowFunction<Tuple2<String, Integer>, Tuple2<String, Integer>, TimeWindow>.Context context, Iterable<Tuple2<String, Integer>> iterable, Collector<Tuple2<String, Integer>> collector) throws Exception {
+            // insert all items of the window into a map
+            for (Tuple2<String, Integer> next : iterable) {
+                String word = next.f0;
+                Integer count = next.f1;
+                map.put(word, count);
+            }
+            // sort map by value
+            map.entrySet()
+                    .stream()
+                    .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                    .forEach(entry -> collector.collect(new Tuple2<>(entry.getKey(),entry.getValue())));
+        }
+    }
+
+    /**
+     * Implements the string tokenizer that splits sentences into words as a user-defined
+     * FlatMapFunction. The function takes a line (String) and splits it into multiple pairs in the
+     * form of "(word,1)" ({@code Tuple2<String, Integer>}).
+     */
+    public static final class Tokenizer implements FlatMapFunction<String, Tuple2<String, Integer>> {
+
+        @Override
+        public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
+            // normalize and split the line
+            String[] tokens = value.toLowerCase().split("\\W+");
+
+            // emit the pairs
+            for (String token : tokens) {
+                if (token.length() > 0) {
+                    out.collect(new Tuple2<>(token, 1));
+                }
+            }
+        }
+    }
+}
